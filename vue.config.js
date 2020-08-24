c2adef81269ff9929a1d73aaf81e2d60f08e799c
zhangjinxu <962485450@qq.com>
2020-08-24 17:11:11 +0800

    ..

--- vue.config.js
+++ vue.config.js
@@ -6,9 +6,9 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'? './': '/',
   outputDir:'docs',//默认dist 打包生成的 文件夹  --no-clean 关闭打包前删除dist文件成操作
   //从生成的资源覆写 filename 或 chunkFilename 时，assetsDir 会被忽略
-  // assetsDir:'', //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+  assetsDir:'', //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
   //指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
-  // indexPath:'index.html',
+  indexPath:'index.html',
   // devServer: {
   //   host:'0.0.0.0',//地址
   //   port:'8090', //项目启动的端口号
