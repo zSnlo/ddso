fdaf7905673d126b7f40099e0d0da8d01cf3375e
zhangjinxu <962485450@qq.com>
2020-08-25 09:08:10 +0800

    8.25修改2

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,37 @@
-// console.log(process.env.NODE_ENV)
+const webpack = require('webpack')
+console.log(process.env.NODE_ENV,process.argv[4])
 // process.env 可以获取当前运行环境的参数
 // development  开发环境  production  运行环境 
-
+// process.env.NODE_ENV=production 运行process.argv[4]=undefined
+let api_root = ''; //网址名称
+let baseURI = ''; //CDN路径
+switch (process.env.NODE_ENV) {
+  case 'development':
+      api_root = "http://www.baidu.com/";
+      baseURI = '/';
+      break;
+  case 'production':
+      api_root = "https://zsnlo.github.io/ddso/";
+      baseURI = './';
+      break;
+  default:
+      break;
+}
+//插件用于在全局获取路径或其他
+var definePlugin = new webpack.DefinePlugin({
+  API_ROOT: JSON.stringify(api_root),
+});
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'? 'https://zsnlo.github.io/ddso/': '/',
+  publicPath: baseURI,
   outputDir:'docs',//默认dist 打包生成的 文件夹  --no-clean 关闭打包前删除dist文件成操作
   //从生成的资源覆写 filename 或 chunkFilename 时，assetsDir 会被忽略
   assetsDir: '', // 静态资源目录 (js, css, img, fonts)
   // assetsPublicPath:'/',
   //指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
   // indexPath:'index.html',
+  transpileDependencies: [], // 默认babel-loader忽略mode_modules，这里可增加例外的依赖包名
+  productionSourceMap: false, // 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度
+  parallel: require('os').cpus().length > 1, // 构建时开启多进程处理babel编译
 
   devServer: {
   //   host:'0.0.0.0',//地址
